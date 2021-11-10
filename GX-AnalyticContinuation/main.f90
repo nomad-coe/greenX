5b7cc5bda4d883ca8d1fd15bd67dbfb8c735cbeb
Maryam <aziziph@gmail.com>
2021-11-10 10:37:23 +0100

    pade approximation

---
+++ GX-AnalyticContinuation/main.f90
@@ -0,0 +1,18 @@
+program  main
+
+    use m_simple_pade
+
+    
+    !write(*,*) pade(n,z,f,zz)
+
+    complex(dpc),parameter :: zz = (0.1,0.0) 
+
+
+    write(*,*) 'pade =', test(), 'original_function=', cos(zz) 
+
+
+end program
+
+
+    
+
