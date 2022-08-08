8f263738d5c0783fd241ede6fd3f86162dfb6290
Alex Buccheri <alexanderbuccheri@googlemail.com>
2022-08-08 14:44:36 +0200

    Expose tau_npoints_supported and reintroduce program to tabulate grids

--- GX-TimeFrequency/api/gx_minimax.f90
+++ GX-TimeFrequency/api/gx_minimax.f90
@@ -13,6 +13,7 @@
 !>
 module gx_minimax
   use mp2_grids, only : gx_minimax_grid
+  use minimax_gw, only : tau_npoints_supported
   use api_utilites, only: gx_check_ntau, gx_get_error_message
  end module gx_minimax     
  
