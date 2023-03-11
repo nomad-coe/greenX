2702a48c58e29ea51616392be71e8d45f1448f73
gonzex <xavier.gonze@uclouvain.be>
2023-03-11 06:37:24 +0100

    Update gx_common.h

--- GX-TimeFrequency/src/gx_common.h
+++ GX-TimeFrequency/src/gx_common.h
@@ -1,4 +1,8 @@
-
+! **************************************************************************************************
+!  Copyright (C) 2020-2023 Green-X library
+!  This file is distributed under the terms of the APACHE2 License.
+!
+! **************************************************************************************************
 #ifdef HAVE_FC_LONG_LINES
 #define _REGISTER_EXC(msg) call register_exc(msg, filename=__FILE__, lineno=__LINE__) 
 #else
