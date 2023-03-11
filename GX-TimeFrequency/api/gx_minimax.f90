5ca538f3ec8f43a93c0b8b016ed594cc9253eca4
gonzex <xavier.gonze@uclouvain.be>
2023-03-11 06:36:47 +0100

    Update gx_minimax.f90

--- GX-TimeFrequency/api/gx_minimax.f90
+++ GX-TimeFrequency/api/gx_minimax.f90
@@ -1,4 +1,8 @@
-!> Public API for minimax library
+! **************************************************************************************************
+!  Copyright (C) 2020-2023 Green-X library
+!  This file is distributed under the terms of the APACHE2 License.
+!
+! **************************************************************************************************!> Public API for minimax library
 !>
 !> Compute minimax grid for RPA energy and GW calculation on imaginary time/frequency domain.
 !> Usage:
